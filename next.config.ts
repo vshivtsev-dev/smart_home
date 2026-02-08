242d2188c9cb2edb3b803189402a6d3e96a09e4a
Aleksandr Vshivtsev <vshivcev.alexandr@gmail.com>
2026-02-08 23:50:59 +0100

    add Dockerfile upd prisma.config.ts remove db from docker-compose

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   output: "standalone",
+  allowedDevOrigins: ["192.168.0.*"],
 };
 
 export default nextConfig;
